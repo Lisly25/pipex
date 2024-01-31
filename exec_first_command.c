6ac511e1303d3c26fff9a34f20abf56275e4facf
Szabina Korbai <skorbai@c1r4p8.hive.fi>
2024-01-31 14:37:04 +0200

    Functions refactored to be less than 25 lines long

--- exec_first_command.c
+++ exec_first_command.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 16:06:20 by skorbai           #+#    #+#             */
-/*   Updated: 2024/01/31 14:22:43 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/01/31 14:35:43 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	exec_first_command(char *file1, char *cmd1, char ***env, int *fd)
 	char	**command;
 	char	*path;
 
-	//ft_printf("In exec first command\n\n\n");
-	//deleteme_arr_print(*env);
 	close(fd[PIPE_READ_END]);
 	file1_fd = open(file1, O_RDONLY);
 	if (file1_fd == -1)
@@ -28,27 +26,17 @@ void	exec_first_command(char *file1, char *cmd1, char ***env, int *fd)
 	dup2(fd[PIPE_WRITE_END], STDOUT_FILENO);
 	close(fd[PIPE_WRITE_END]);
 	close(file1_fd);
-	//char *input = get_next_line(STDIN_FILENO);
-	//ft_putstr_fd("we are here\n",2);
-	//ft_putstr_fd(input, 2);
 	command = ft_split(cmd1, ' ');
 	if (command == NULL)
-		ft_message_and_exit("Malloc fail");
+		ft_message_and_exit("Error: Malloc fail");
 	path = find_correct_path(command, env);
-	if (path == NULL)
-		ft_free_and_exit("Error: no valid path for execution", command);
-	//ft_putstr_fd(path, 2);
-	while (execve(path, command, *env) == -1)
+	while (path != NULL)
 	{
+		execve(path, command, *env);
 		free(path);
 		path = find_correct_path(command, env);
-		//ft_putstr_fd("\nNext path to check: ", 2);
-		//ft_putstr_fd(path, 2);
 		if (path == NULL)
 			break ;
 	}
-	free_2d_array(command);
-	free(path);
-	ft_message_and_exit("Failed to execute command1");
-	exit(0);
+	ft_free_and_exit("Failed to execute command1", command);
 }
